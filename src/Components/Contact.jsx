020e7819f7bc1f00738f3415958fc7a719859d9f
rizeenf <rize.nf11@gmail.com>
2023-07-04 00:32:14 +0700

    Adding EmailJS

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useRef } from "react";
 import arrow from "../assets/arrow.svg";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_6ybgowr",
+        "template_bhroiqi",
+        form.current,
+        "ISchR0uKrdcTJau1I"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    const reset = document.getElementsByName("formMsg")[0];
+
+    setInterval(() => {
+      reset.reset();
+    }, 1000);
+  };
+
   return (
     <div className=" h-screen bg-yellow-500 text-white">
       <div className="header flex justify-center items-center flex-col h-1/6">
@@ -14,28 +42,38 @@ const Contact = () => {
       <div className="main flex flex-row justify-center items-center h-5/6  ">
         <div className="left flex justify-center items-center flex-col flex-1 h-full  ">
           <form
+            ref={form}
+            onSubmit={sendEmail}
             action=""
+            name="formMsg"
             className=" w-full flex justify-center items-center flex-col gap-5 "
           >
+            <span className=" text-xl">
+              Your Message will sent using EmailJS
+            </span>
             <input
               type="text"
               placeholder="Your name.."
-              className=" bg-white rounded p-3 text-white h-16 w-1/2 border-b-gray-400 border-b-2"
+              name="sender_name"
+              className=" bg-white rounded p-3  h-16 w-1/2 border-b-gray-400 border-b-2 text-gray-600"
             />
             <input
               type="email"
               placeholder="Email.."
-              className=" bg-white rounded p-3 text-white h-16 w-1/2 border-b-gray-400 border-b-2"
+              name="sender_email"
+              className=" bg-white rounded p-3  h-16 w-1/2 border-b-gray-400 border-b-2 text-gray-600"
             />
             <input
               type="text"
               placeholder="Message.."
-              className=" bg-white rounded p-3 text-white w-1/2 h-52 border-b-gray-400 border-b-2"
+              name="message"
+              className=" bg-white rounded p-3  w-1/2 h-52 border-b-gray-400 border-b-2 text-gray-600"
             />
             <input
               type="submit"
               value="Send me a Message"
-              className=" bg-yellow-100 p-3 w-1/3 text-gray-500 rounded"
+              name="message"
+              className=" bg-yellow-100 p-3 w-1/3 text-gray-500 rounded cursor-pointer"
             />
           </form>
         </div>
@@ -51,7 +89,7 @@ const Contact = () => {
                 <span>Email</span>
                 <span>rize.nf11@gmail.com</span>
               </div>
-              <div className="top flex  flex-col justify-center items-center h-1/2 w-full">
+              <div className="top flex  flex-col justify-center items-center h-1/2 w-full">git 
                 <img src={arrow} alt="" className=" font-bold w-14 h-14" />
                 <span>Location</span>
                 <span>Bekasi, Jawa Barat</span>
